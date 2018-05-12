fdab2887aed086fcf22fb4dc381b94e27ec0817f
Grant Nelson <grantmnelsn@gmail.com>
2018-05-12 16:00:02 -0400

    env fetch

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -2,7 +2,7 @@ class PdfController < ApplicationController
 
   def retrieve
     case params[:key]
-    when ENV['PDF_ACCESS_KEY']
+    when ENV.fetch('PDF_ACCESS_KEY')
       url = params[:url]
       text = Yomu.new(url).text
       render plain: text
