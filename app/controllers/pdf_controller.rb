86990ba123ae92dec9f46dffdce80ab28bb2aaa6
Grant Nelson <grantmnelsn@gmail.com>
2018-08-13 21:37:39 -0400

    simple space-to-%20 replacement

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -3,7 +3,7 @@ class PdfController < ApplicationController
   def retrieve
     case params[:key]
     when ENV.fetch('PDF_ACCESS_KEY')
-      url = ERB::Util.url_encode(params[:url])
+      url = params[:url].gsub(" ", "%20")
       text = Henkei.new(url).text
       render plain: text
     else
