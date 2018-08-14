131129040e7163fe2d29675024566a880a8d7c7e
Grant Nelson <grantmnelsn@gmail.com>
2018-08-13 21:30:28 -0400

    added ERB namespace

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -3,7 +3,7 @@ class PdfController < ApplicationController
   def retrieve
     case params[:key]
     when ENV.fetch('PDF_ACCESS_KEY')
-      url = url_encode(params[:url])
+      url = ERB::Util.url_encode(params[:url])
       text = Henkei.new(url).text
       render plain: text
     else
